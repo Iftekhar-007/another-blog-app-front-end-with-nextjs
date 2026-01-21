1321b1371d01e16bd8e8a7b2369170dd8229a1ba
Iftekhar-007 <shawonsp534@gmail.com>
2026-01-21 15:37:16 +0600

    better auth added

--- src/app/(commonlayout)/auth/login/page.tsx
+++ src/app/(commonlayout)/auth/login/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { LoginForm } from "@/components/modules/auth/login-form";
 
-const LoginPage = () => {
-  return <div>this is login page content</div>;
-};
-
-export default LoginPage;
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
