08353082ac49624804467adc408087b918f023cc
Iftekhar-007 <shawonsp534@gmail.com>
2026-01-22 15:25:04 +0600

    ok

--- src/app/(commonlayout)/page.tsx
+++ src/app/(commonlayout)/page.tsx
@@ -1,15 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
 import { cookies } from "next/headers";
 
 export default async function Home() {
   const cookieStore = await cookies();
 
-  console.log(cookieStore);
   const res = await fetch("http://localhost:5000/api/auth/get-session", {
     headers: {
       Cookie: cookieStore.toString(),
     },
+    cache: "no-store",
   });
   const session = await res.json();
 
