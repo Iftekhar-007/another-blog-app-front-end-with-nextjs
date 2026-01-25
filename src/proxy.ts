d9df1353dc8574a4e091e3c497cd39569b96f630
Iftekhar-007 <shawonsp534@gmail.com>
2026-01-25 18:37:17 +0600

    comment done added

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,21 +3,30 @@ import { userService } from "./services/user.service";
 import { Roles } from "./constants/roles";
 
 export async function proxy(request: NextRequest) {
+  // get pathname
   const pathName = request.nextUrl.pathname;
 
   let isAuthenticated = false;
   let isAdmin = false;
 
+  // ! get user data
+
   const { data: session } = await userService.getSession();
+
+  // ! set user data
 
   if (session) {
     isAuthenticated = true;
     isAdmin = session.user.role === Roles.admin;
   }
 
+  // valiadte user role with admin role
+
   if (!isAuthenticated) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
+
+  // ! redirect users to their role based page based on users role
 
   if (isAdmin && pathName.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/admin-dashboard", request.url));
